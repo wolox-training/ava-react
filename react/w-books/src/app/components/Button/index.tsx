f50ef453706adf9ce86d04e65a7966e8978432e2
Agustin Vazquez <agustinvazquez453@gmail.com>
2021-01-18 12:32:57 -0300

    changed button to receive children instead of text

--- react/w-books/src/app/components/Button/index.tsx
+++ react/w-books/src/app/components/Button/index.tsx
@@ -1,20 +1,20 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
 
 import styles from './styles.module.scss';
 
 interface ButtonProps {
-  buttonText: string;
+  children: ReactNode;
   className?: string;
   isFilled?: boolean;
   isSubmit?: boolean;
 }
 
-export default function Button({ buttonText, isSubmit, className, isFilled }: ButtonProps) {
+export default function Button({ children, isSubmit, className, isFilled }: ButtonProps) {
   return (
     <button
       type={isSubmit ? 'submit' : 'button'}
       className={`${styles.button} ${isFilled && styles.filled} ${className}`}>
-      {buttonText}
+      {children}
     </button>
   );
 }
