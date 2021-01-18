75116557b118fc1d250063d1c57616ab33c140b4
Agustin Vazquez <agustinvazquez453@gmail.com>
2021-01-18 12:43:37 -0300

    add className prop to logoWolox

--- react/w-books/src/app/components/LogoWolox/index.tsx
+++ react/w-books/src/app/components/LogoWolox/index.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import imgLogoWolox from '../../assets/img/logo-wolox.png';
 
-function LogoWolox(){
-  return  <img src={imgLogoWolox} alt="The logo of Wolox"/>;
+interface LogoWoloxProps {
+  className?: string;
+}
+
+function LogoWolox({className}:LogoWoloxProps){
+  return  <img src={imgLogoWolox} alt="The logo of Wolox" className={className}/>;
 }
 
 export default LogoWolox;
