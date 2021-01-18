a4a092d4a873e237ada055afab1121ed87d66497
Agustin Vazquez <agustinvazquez453@gmail.com>
2021-01-18 10:54:52 -0300

    add wolox logo component

---
+++ react/w-books/src/app/components/LogoWolox/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import imgLogoWolox from '../../assets/img/logo-wolox.png';
+
+function LogoWolox(){
+  return  <img src={imgLogoWolox} alt="The logo of Wolox"/>;
+}
+
+export default LogoWolox;
