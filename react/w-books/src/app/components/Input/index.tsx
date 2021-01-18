5e1b4cfdaab35b2cf3dd346c2920ef355fe031ac
Agustin Vazquez <agustinvazquez453@gmail.com>
2021-01-18 16:19:06 -0300

    make type customizable

--- react/w-books/src/app/components/Input/index.tsx
+++ react/w-books/src/app/components/Input/index.tsx
@@ -2,14 +2,15 @@ import React, { ChangeEvent } from 'react';
 
 import styles from './styles.module.scss';
 
-interface InputProps {
+export interface InputProps {
   className?: string;
   id: string;
+  type: string;
   handleChange: (event: ChangeEvent<HTMLInputElement>) => void;
 }
 
-function Input({ className, id, handleChange }: InputProps): JSX.Element {
-  return <input type="text" id={id} className={`${styles.input} ${className}`} onChange={handleChange}/>;
+function Input({ className, id, type, handleChange }: InputProps): JSX.Element {
+  return <input type={type} id={id} className={`${styles.input} ${className}`} onChange={handleChange}/>;
 }
 
 export default Input;
