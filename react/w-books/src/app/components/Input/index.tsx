130e6322e4800dc503af4b85e199e6ebd4949e99
Agustin Vazquez <agustinvazquez453@gmail.com>
2021-01-18 10:54:30 -0300

    modify input component to receive className

--- react/w-books/src/app/components/Input/index.tsx
+++ react/w-books/src/app/components/Input/index.tsx
@@ -2,8 +2,12 @@ import React from 'react';
 
 import styles from './styles.module.scss';
 
-function Input() {
-  return <input type="text" className={styles.input} />;
+interface InputProps {
+  className?: string;
+}
+
+function Input({ className }: InputProps): JSX.Element {
+  return <input type="text" className={`${styles.input} ${className}`} />;
 }
 
 export default Input;
