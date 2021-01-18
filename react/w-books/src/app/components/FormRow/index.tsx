da2215beda597713f84ce7278b588571f75e35e9
Agustin Vazquez <agustinvazquez453@gmail.com>
2021-01-18 15:51:44 -0300

    add props to manage input data

--- react/w-books/src/app/components/FormRow/index.tsx
+++ react/w-books/src/app/components/FormRow/index.tsx
@@ -1,20 +1,22 @@
-import React, { Component } from 'react';
+import React, { ChangeEvent } from 'react';
 
 import Input from '../Input';
 import styles from './styles.module.scss';
 
 interface FormRowProps {
   labelName: string;
+  id: string;
   className?: string;
+  handleChange: (event: ChangeEvent<HTMLInputElement>) => void;
 }
 
-function FormRow({ labelName, className }: FormRowProps): JSX.Element {
+function FormRow({ labelName, className, id, handleChange }: FormRowProps): JSX.Element {
   return (
     <div className={`${styles["form-row"]} ${className}`}>
       <label htmlFor="" className={styles["form-row-label"]}>
         {labelName}
       </label>
-      <Input className={styles["form-row-input"]} />
+      <Input className={styles["form-row-input"]} id={id} handleChange={handleChange}/>
     </div>
   );
 }
--- react/w-books/src/app/components/Input/index.tsx
+++ react/w-books/src/app/components/Input/index.tsx
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { ChangeEvent } from 'react';
 
 import styles from './styles.module.scss';
 
 interface InputProps {
   className?: string;
+  id: string;
+  handleChange: (event: ChangeEvent<HTMLInputElement>) => void;
 }
 
-function Input({ className }: InputProps): JSX.Element {
-  return <input type="text" className={`${styles.input} ${className}`} />;
+function Input({ className, id, handleChange }: InputProps): JSX.Element {
+  return <input type="text" id={id} className={`${styles.input} ${className}`} onChange={handleChange}/>;
 }
 
 export default Input;
