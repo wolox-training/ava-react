70c984c6b3eb12e9b913e8f3099721303a23947e
Agustin Vazquez <agustinvazquez453@gmail.com>
2021-01-18 16:20:37 -0300

    add types to inputs and save form data in state

--- react/w-books/src/app/screens/SignUp/components/SignUpForm/index.tsx
+++ react/w-books/src/app/screens/SignUp/components/SignUpForm/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { ChangeEvent, Component } from 'react';
 
 import styles from './styles.module.scss';
 
@@ -6,15 +6,88 @@ import Form from '../../../../components/Form';
 import FormRow from '../../../../components/FormRow';
 import Button from '../../../../components/Button';
 
-export default function SignUpForm() {
-  return (
-    <Form className={styles["sign-up-form"]}>
-      <FormRow labelName="Nombre" className={styles["form-row"]} />
-      <FormRow labelName="Apellido" className={styles["form-row"]} />
-      <FormRow labelName="Email" className={styles["form-row"]} />
-      <FormRow labelName="Password" className={styles["form-row"]} />
-      <FormRow labelName="Confirmación de Password" className={styles["form-row"]} />
-      <Button isFilled isSubmit>Sign Up</Button>
-    </Form>
-  )
+interface SignUpFormState {
+  formData: UserData;
 }
+
+interface UserData {
+  email: string;
+  password: string;
+  password_confirmation: string;
+  first_name: string;
+  last_name: string;
+  locale: string;
+}
+export default class SignUpForm extends Component<{}, SignUpFormState> {
+
+  handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+    if (event?.target?.id) {
+      this.setState(
+        { formData: { ...this.state?.formData, [event.target.id]: event.target.value } }
+      );
+    }
+  }
+
+  signUp = () => {
+    if (this.state && this.state.formData) {
+      const data = { ...this.state.formData, locale: navigator.language };
+
+      console.log(data);
+    }
+  }
+
+  render() {
+    return (
+      <Form className={styles["sign-up-form"]} handleSubmit={this.signUp}>
+        <FormRow
+          labelName="Nombre"
+          className={styles["form-row"]}
+          inputProps={{
+            id: "first_name",
+            handleChange: this.handleInputChange,
+            type: "text"
+          }}
+        />
+        <FormRow
+          labelName="Apellido"
+          className={styles["form-row"]}
+          inputProps={{
+            id: "last_name",
+            handleChange: this.handleInputChange,
+            type: "text"
+          }}
+        />
+        <FormRow
+          labelName="Email"
+          className={styles["form-row"]}
+          inputProps={{
+            id: "email",
+            handleChange: this.handleInputChange,
+            type: "email"
+          }}
+        />
+        <FormRow
+          labelName="Password"
+          className={styles["form-row"]}
+          inputProps={{
+            id: "password",
+            handleChange: this.handleInputChange,
+            type: "password"
+          }}
+        />
+        <FormRow
+          labelName="Confirmación de Password"
+          className={styles["form-row"]}
+          inputProps={{
+            id: "password_confirmation",
+            handleChange: this.handleInputChange,
+            type: "password"
+          }}
+        />
+
+        <Button isFilled isSubmit>Sign Up</Button>
+      </Form>
+    )
+  }
+}
+
