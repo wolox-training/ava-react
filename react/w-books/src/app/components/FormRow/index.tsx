57ac3725f4d1d62816d66cfa81e4fc9e17c6b619
Agustin Vazquez <agustinvazquez453@gmail.com>
2021-01-18 16:19:59 -0300

    move input props to inputProps

--- react/w-books/src/app/components/FormRow/index.tsx
+++ react/w-books/src/app/components/FormRow/index.tsx
@@ -1,22 +1,21 @@
 import React, { ChangeEvent } from 'react';
 
-import Input from '../Input';
+import Input, { InputProps } from '../Input';
 import styles from './styles.module.scss';
 
 interface FormRowProps {
-  labelName: string;
-  id: string;
   className?: string;
-  handleChange: (event: ChangeEvent<HTMLInputElement>) => void;
+  labelName: string;
+  inputProps: InputProps;
 }
 
-function FormRow({ labelName, className, id, handleChange }: FormRowProps): JSX.Element {
+function FormRow({ labelName, className, inputProps }: FormRowProps): JSX.Element {
   return (
     <div className={`${styles["form-row"]} ${className}`}>
       <label htmlFor="" className={styles["form-row-label"]}>
         {labelName}
       </label>
-      <Input className={styles["form-row-input"]} id={id} handleChange={handleChange}/>
+      <Input className={styles["form-row-input"]} {...inputProps} />
     </div>
   );
 }
