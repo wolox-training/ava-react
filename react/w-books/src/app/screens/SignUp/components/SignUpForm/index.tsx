d5a2ba2270382c9f954ca98d7c07c3aff192f326
Agustin Vazquez <agustinvazquez453@gmail.com>
2021-01-19 12:38:14 -0300

    migrate to functional components

--- react/w-books/src/app/screens/SignUp/components/SignUpForm/index.tsx
+++ react/w-books/src/app/screens/SignUp/components/SignUpForm/index.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEvent, Component } from 'react';
+import React, { ChangeEvent, useState } from 'react';
 
 import Form from '../../../../components/Form';
 import FormRow from '../../../../components/FormRow';
@@ -6,91 +6,85 @@ import Button from '../../../../components/Button';
 
 import styles from './styles.module.scss';
 
-interface SignUpFormState {
-  formData: UserData;
+interface UserData {
+  email?: string;
+  password?: string;
+  password_confirmation?: string;
+  first_name?: string;
+  last_name?: string;
+  locale?: string;
 }
 
-interface UserData {
-  email: string;
-  password: string;
-  password_confirmation: string;
-  first_name: string;
-  last_name: string;
-  locale: string;
-}
-export default class SignUpForm extends Component<{}, SignUpFormState> {
-  handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+export default function SignUpForm() {
+  const [formData, setFormData] = useState<UserData | null>(null);
+
+  const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
     if (event?.target?.id) {
       event.persist();
 
-      this.setState((actualState) => (
-        {
-          formData: { ...actualState?.formData, [event.target.id]: event.target.value }
-        }
-      ));
+      setFormData((actualFormData: UserData | null) =>
+        ({ ...actualFormData, [event.target.id]: event.target.value })
+      );
     }
   }
 
-  signUp = () => {
-    if (this.state && this.state.formData) {
-      const data = { ...this.state.formData, locale: navigator.language };
+  const signUp = () => {
+    if (formData) {
+      const data = { ...formData, locale: navigator.language };
 
       console.log(data);
     }
   }
 
-  render() {
-    return (
-      <Form className={styles['sign-up-form']} handleSubmit={this.signUp}>
-        <FormRow
-          labelName="Nombre"
-          className={styles['form-row']}
-          inputProps={{
-            id: 'first_name',
-            handleChange: this.handleInputChange,
-            type: 'text'
-          }}
-        />
-        <FormRow
-          labelName="Apellido"
-          className={styles['form-row']}
-          inputProps={{
-            id: 'last_name',
-            handleChange: this.handleInputChange,
-            type: 'text'
-          }}
-        />
-        <FormRow
-          labelName="Email"
-          className={styles['form-row']}
-          inputProps={{
-            id: 'email',
-            handleChange: this.handleInputChange,
-            type: 'email'
-          }}
-        />
-        <FormRow
-          labelName="Password"
-          className={styles['form-row']}
-          inputProps={{
-            id: 'password',
-            handleChange: this.handleInputChange,
-            type: 'password'
-          }}
-        />
-        <FormRow
-          labelName="Confirmación de Password"
-          className={styles['form-row']}
-          inputProps={{
-            id: 'password_confirmation',
-            handleChange: this.handleInputChange,
-            type: 'password'
-          }}
-        />
+  return (
+    <Form className={styles['sign-up-form']} handleSubmit={signUp}>
+      <FormRow
+        labelName="Nombre"
+        className={styles['form-row']}
+        inputProps={{
+          id: 'first_name',
+          handleChange: handleInputChange,
+          type: 'text'
+        }}
+      />
+      <FormRow
+        labelName="Apellido"
+        className={styles['form-row']}
+        inputProps={{
+          id: 'last_name',
+          handleChange: handleInputChange,
+          type: 'text'
+        }}
+      />
+      <FormRow
+        labelName="Email"
+        className={styles['form-row']}
+        inputProps={{
+          id: 'email',
+          handleChange: handleInputChange,
+          type: 'email'
+        }}
+      />
+      <FormRow
+        labelName="Password"
+        className={styles['form-row']}
+        inputProps={{
+          id: 'password',
+          handleChange: handleInputChange,
+          type: 'password'
+        }}
+      />
+      <FormRow
+        labelName="Confirmación de Password"
+        className={styles['form-row']}
+        inputProps={{
+          id: 'password_confirmation',
+          handleChange: handleInputChange,
+          type: 'password'
+        }}
+      />
 
-        <Button isFilled isSubmit>Sign Up</Button>
-      </Form>
-    );
-  }
+      <Button isFilled isSubmit>Sign Up</Button>
+    </Form>
+  );
 }
-
