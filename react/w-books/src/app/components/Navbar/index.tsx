54858cc90bcbfbdfcbc245d294d5b01a74d2d4c5
Agustin Vazquez <agustinvazquez453@gmail.com>
2021-01-21 12:39:32 -0300

    add navbar component

--- react/w-books/src/app/components/Navbar/index.tsx
+++ react/w-books/src/app/components/Navbar/index.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import LogoWolox from '~components/LogoWolox';
+
+import styles from './styles.module.scss';
 
 export default function Navbar() {
   return (
-    <div>
-      Navbar
-    </div>
+    <nav className={styles.nav}>
+      <LogoWolox className={styles.navLogo}/>
+      <a href="/login.html" className={styles.navOption}>Logout</a>
+    </nav>
   )
 }
