626f07fe3d4d7b91368fc72941c797467662c7e9
Agustin Vazquez <agustinvazquez453@gmail.com>
2021-01-21 15:57:03 -0300

    do async tests

--- react/finger-exercises/testing_1/02 - async/test.js
+++ react/finger-exercises/testing_1/02 - async/test.js
@@ -1,13 +1,25 @@
 import getData from '.';
 
 describe('02 - async', () => {
-  xit('getData resolves the data if true is sent as argument (use async/await)', async () => {
-    getData();
+  it('getData resolves the data if true is sent as argument (use async/await)', async () => {
+    const result = await getData(true);
+    expect(result).toBe('data');
   });
-  xit('getData resolves the data if true is sent as argument (avoid async/await)', () => {
+  it('getData resolves the data if true is sent as argument (avoid async/await)', () => {
+    return getData(true).then(result => expect(result).toBe('data'));
   });
-  xit('getData throws error if false is sent as argument (use async/await)', async () => {
+  it('getData throws error if false is sent as argument (use async/await)', async () => {
+    try {
+      const result = await getData(false);
+      expect(result).not.toBe('data');
+    }
+    catch (err) {
+      expect(err).toEqual(new Error('error'));
+    }
   });
-  xit('getData throws error if false is sent as argument (avoid async/await)', () => {
+  it('getData throws error if false is sent as argument (avoid async/await)', () => {
+    return getData(false).catch(err => {
+      expect(err).toEqual(new Error('error'));
+    });
   });
 });
