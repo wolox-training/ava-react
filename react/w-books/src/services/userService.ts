a83ffd8e9ccfb066eae25858ea7b334942f55033
Agustin Vazquez <agustinvazquez453@gmail.com>
2021-01-21 11:10:31 -0300

    fix sign up api route

--- react/w-books/src/services/userService.ts
+++ react/w-books/src/services/userService.ts
@@ -2,7 +2,7 @@ import { ApiResponse } from 'apisauce';
 import api from '../config/api';
 
 export const SIGN_UP = '';
-export const LOGIN = 'sign_in';
+export const LOGIN = '/sign_in';
 
 export async function postUser(action: typeof SIGN_UP | typeof LOGIN, user:any):Promise<ApiResponse<any, any>> {
   return api.post(`users${action}`, user);
