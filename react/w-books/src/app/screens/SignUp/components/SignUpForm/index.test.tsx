590639e1798ca6db89090e6da6c5b28364ae81f6
Agustin Vazquez <agustinvazquez453@gmail.com>
2021-01-22 13:26:29 -0300

    tests for sign up form

--- react/w-books/src/app/screens/SignUp/components/SignUpForm/index.test.tsx
+++ react/w-books/src/app/screens/SignUp/components/SignUpForm/index.test.tsx
@@ -8,114 +8,125 @@ import { act } from 'react-dom/test-utils';
 
 import SignUpForm from './';
 
-const CorrectUserData = {
-  email: 'agustin.vazquez@wolox.com.ar',
-  password: '123456',
-  password_confirmation: '123456',
-  first_name: 'Agustin',
-  last_name: 'Vazquez'
-};
+const cases = [
+  {
+    name: 'Puts correct data and execute handleSignUp',
+    validate: (mockSubmit: any, formElement: any, initialData: any) => {
+      expect(mockSubmit).toHaveBeenCalled();
 
-const InvalidCase1UserData = {
-  email: '',
-  password: '',
-  password_confirmation: '',
-  first_name: '',
-  last_name: ''
-};
+      const loginData = mockSubmit.mock.calls[0][0];
+      expect(loginData).toEqual(initialData);
+    },
+    data: {
+      email: 'agustin.vazquez@wolox.com.ar',
+      password: '123456',
+      password_confirmation: '123456',
+      first_name: 'Agustin',
+      last_name: 'Vazquez'
+    }
+  },
+  {
+    name: 'Insert empty data and must be showed an error in screen',
+    validate: (mockSubmit: any, formElement: any, initialData: any) => {
+      expect(mockSubmit).not.toHaveBeenCalled();
+      expect(formElement).toHaveTextContent('FormValidation:Required');
+    },
+    data: {
+      email: '',
+      password: '',
+      password_confirmation: '',
+      first_name: '',
+      last_name: ''
+    }
+  },
+  {
+    name: 'Insert an short password and must be showed an error in screen',
+    validate: (mockSubmit: any, formElement: any, initialData: any) => {
+      expect(mockSubmit).not.toHaveBeenCalled();
+      expect(formElement).toHaveTextContent('FormValidation:MinLength');
+    },
+    data: {
+      email: 'agustin.vazquez@wolox.com.ar',
+      password: '123',
+      password_confirmation: '123456',
+      first_name: 'Agustin',
+      last_name: 'Vazquez'
+    }
+  },
+  {
+    name: 'Insert an long name and must be showed an error in screen',
+    validate: (mockSubmit: any, formElement: any, initialData: any) => {
+      expect(mockSubmit).not.toHaveBeenCalled();
+      expect(formElement).toHaveTextContent('FormValidation:MaxLength');
+    },
+    data: {
+      email: 'agustin.vazquez@wolox.com.ar',
+      password: '123456',
+      password_confirmation: '123456',
+      first_name: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Adipisci, ducimus ut quam laboriosam hic nam placeat quidem? Iure, voluptas quaerat. Voluptatem suscipit nam architecto, praesentium aliquam quae! Corporis, soluta dolore!',
+      last_name: 'Vazquez'
+    }
+  },
+  {
+    name: 'Insert an long last name and must be showed an error in screen',
+    validate: (mockSubmit: any, formElement: any, initialData: any) => {
+      expect(mockSubmit).not.toHaveBeenCalled();
+      expect(formElement).toHaveTextContent('FormValidation:MaxLength');
+    },
+    data: {
+      email: 'agustin.vazquez@wolox.com.ar',
+      password: '123456',
+      password_confirmation: '123456',
+      first_name: 'Agustin',
+      last_name: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Adipisci, ducimus ut quam laboriosam hic nam placeat quidem? Iure, voluptas quaerat. Voluptatem suscipit nam architecto, praesentium aliquam quae! Corporis, soluta dolore!'
+    }
+  },
+  {
+    name: 'Insert an different passworn in confirmation password field and must be showed an error in screen',
+    validate: (mockSubmit: any, formElement: any, initialData: any) => {
+      expect(mockSubmit).not.toHaveBeenCalled();
+      expect(formElement).toHaveTextContent('FormValidation:Validate');
+    },
+    data: {
+      email: 'agustin.vazquez@wolox.com.ar',
+      password: '123456',
+      password_confirmation: '123456x',
+      first_name: 'Agustin',
+      last_name: 'Vazquez'
+    }
+  },
 
-const InvalidCase2UserData = {
-  email: 'agustin.vazquez@wolox.com.ar',
-  password: '123',
-  password_confirmation: '123456',
-  first_name: 'Agustin',
-  last_name: 'Vazquez'
-};
+];
 
+it('Components are rendered', () => {
+  render(<SignUpForm testId="SignUpForm" />, { wrapper: MemoryRouter });
 
-const InvalidCase3UserData = {
-  email: 'agustin.vazquez@wolox.com.ar',
-  password: '123456',
-  password_confirmation: '123456',
-  first_name: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Adipisci, ducimus ut quam laboriosam hic nam placeat quidem? Iure, voluptas quaerat. Voluptatem suscipit nam architecto, praesentium aliquam quae! Corporis, soluta dolore!', 
-  last_name: 'Vazquez'
-};
+  expect(screen.getByTestId('SignUpForm')).toBeInTheDocument();
+  expect(screen.getByTestId('email')).toBeInTheDocument();
+  expect(screen.getByTestId('password')).toBeInTheDocument();
+  expect(screen.getByTestId('first_name')).toBeInTheDocument();
+  expect(screen.getByTestId('last_name')).toBeInTheDocument();
+  expect(screen.getByTestId('password_confirmation')).toBeInTheDocument();
+});
 
-const InvalidCase4UserData = {
-  email: 'agustin.vazquez@wolox.com.ar',
-  password: '123456',
-  password_confirmation: '123456',
-  first_name: 'Agustin', 
-  last_name: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Adipisci, ducimus ut quam laboriosam hic nam placeat quidem? Iure, voluptas quaerat. Voluptatem suscipit nam architecto, praesentium aliquam quae! Corporis, soluta dolore!'
-};
+describe('Tests to sign up form', () => {
+  cases.map(oCase => {
+    return it(oCase.name, async () => {
+      const { mockSubmit, formElement } = await testUser(oCase.data);
+      await act(async () => {
+        fireEvent.submit(formElement);
+      })
 
-const InvalidCase5UserData = {
-  email: 'agustin.vazquez@wolox.com.ar',
-  password: '123456',
-  password_confirmation: '123456x',
-  first_name: 'Agustin',
-  last_name: 'Vazquez'
-};
-
-describe('Login form tests', () => {
-  it('Components are rendered', () => {
-    render(<SignUpForm testId="SignUpForm" />, { wrapper: MemoryRouter });
-
-    expect(screen.getByTestId('SignUpForm')).toBeInTheDocument();
-    expect(screen.getByTestId('email')).toBeInTheDocument();
-    expect(screen.getByTestId('password')).toBeInTheDocument();
-    expect(screen.getByTestId('first_name')).toBeInTheDocument();
-    expect(screen.getByTestId('last_name')).toBeInTheDocument();
-    expect(screen.getByTestId('password_confirmation')).toBeInTheDocument();
-  });
-
-  it('Puts correct data and execute handleSignUp', async () => {
-
-    const { mockSignUp, signUpForm } = await testUser(CorrectUserData);
-
-    await act(async () => {
-      fireEvent.submit(signUpForm);
-    })
-
-    expect(mockSignUp).toHaveBeenCalled();
-
-    const loginData = mockSignUp.mock.calls[0][0];
-    expect(loginData).toEqual(CorrectUserData);
-  });
-
-  it('Insert empty data and must be showed an error in screen', async () => {
-
-    const { mockSignUp, signUpForm } = await testUser(InvalidCase1UserData);
-
-    await act(async ()=> {
-      fireEvent.submit(signUpForm);
-    })
-
-    expect(mockSignUp).not.toHaveBeenCalled();
-
-    expect(signUpForm).toHaveTextContent('FormValidation:Required');
-  });
-
-  it('insert an short password and must be showed an error in screen', async () => {
-
-    const { mockSignUp, signUpForm } = await testUser(InvalidCase1UserData);
-
-    await act(async ()=> {
-      fireEvent.submit(signUpForm);
-    })
-
-    expect(mockSignUp).not.toHaveBeenCalled();
-
-    expect(signUpForm).toHaveTextContent('FormValidation:Required');
-  });
-
+      oCase.validate(mockSubmit, formElement, oCase.data);
+    });
+  })
 });
 
 const testUser = async (user: any) => {
-  const mockSignUp = jest.fn();
-  render(<SignUpForm testId="LoginForm" handleSignUp={mockSignUp} />, { wrapper: MemoryRouter });
+  const mockSubmit = jest.fn();
+  render(<SignUpForm testId="LoginForm" handleSignUp={mockSubmit} />, { wrapper: MemoryRouter });
 
-  const signUpForm = screen.getByTestId('LoginForm');
+  const formElement = screen.getByTestId('LoginForm');
   const inputEmail = screen.getByTestId('email');
   const inputPassword = screen.getByTestId('password');
   const inputConfirmPassword = screen.getByTestId('password_confirmation');
@@ -131,7 +142,7 @@ const testUser = async (user: any) => {
   })
 
   return {
-    mockSignUp,
-    signUpForm
+    mockSubmit,
+    formElement
   }
 }
