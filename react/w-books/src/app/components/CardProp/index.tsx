d2bb7f49110a8d99aa3b5718b824a6f56e521672
Agustin Vazquez <agustinvazquez453@gmail.com>
2021-01-25 11:56:53 -0300

    modify card prop to get keepVisibleSimple

--- react/w-books/src/app/components/CardProp/index.tsx
+++ react/w-books/src/app/components/CardProp/index.tsx
@@ -3,18 +3,23 @@ import React from 'react'
 
 import styles from './styles.module.scss';
 
-interface CardPropProps {
+export interface iCardProp {
+  keepVisibleSimple?: boolean;
   propName: string;
   value: string;
+}
+
+interface CardPropProps extends iCardProp {
   className?: string;
   isSimple?: boolean;
 }
 
-export default function CardProp({ className, propName, value, isSimple }: CardPropProps) {
+export default function CardProp({ className, propName, value, isSimple, keepVisibleSimple }: CardPropProps) {
   return (
     <div className={
       clsx(styles.cardProp, className, {
-        [styles.simple]: isSimple
+        [styles.simple]: isSimple,
+        [styles.occult] : (isSimple && !keepVisibleSimple)
       })
     }>
       <b className={styles.cardPropName}>{propName}</b>
